16102c66d101ff446b3d0e5bc6a8e964ff080422
farwa-faraz <99012312+farwa-faraz@users.noreply.github.com>
2023-01-30 15:48:08 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,33 @@ function setup()
 {
     canvas = createCanvas(400, 400);
     canvas.center();
+    webcam = createCapture(VIDEO);
+    webcam.hide();
+    poseNet = ml5.poseNet(webcam, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function draw()
+{
+    image(webcam, 0, 0, 400, 400);
 }
 
 function snapshot()
 {
     save("clown.png")
+}
+
+function modelLoaded()
+{
+    console.log("poseNet is initialised");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("x value of nose"+ results[0].pose.nose.x);
+        console.log("y value of nose"+ results[0].pose.nose.y);
+    }
 }
