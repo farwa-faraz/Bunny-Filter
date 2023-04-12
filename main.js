f59f17113198146a1ce44c11ea6957ed76d9ebf5
farwa-faraz <99012312+farwa-faraz@users.noreply.github.com>
2023-04-12 12:04:51 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+function preload()
+{
+    left_ear = loadImage('https://i.postimg.cc/WzbWx9bx/Right-ear-removebg-preview.png');
+    right_ear = loadImage('https://i.postimg.cc/6qpcMY9q/Left-ear-removebg-preview.png');
+    glasses = loadImage('https://i.postimg.cc/FKvBjbJd/Glasses-removebg-preview.png');
+}
+
 function setup()
 {
     canvas = createCanvas(400, 400);
@@ -11,11 +18,17 @@ function setup()
 function draw()
 {
     image(webcam, 0, 0, 400, 400);
+    fill("red");
+    stroke("red");
+    //circle(left_earx, left_eary, 20);
+    image(left_ear, left_earx, left_eary, 160, 160);
+    image(right_ear, right_earx, right_eary, 160, 160);
+    image(glasses, glasses_x, glasses_y, 180, 180);
 }
 
 function snapshot()
 {
-    save("clown.png")
+    save("bunny.png")
 }
 
 function modelLoaded()
@@ -23,12 +36,34 @@ function modelLoaded()
     console.log("poseNet is initialised");
 }
 
+left_earx = 0
+left_eary = 0
+
+right_earx = 0
+right_eary = 0
+
+glasses_x = 0
+glasses_y = 0
+
 function gotPoses(results)
 {
     if(results.length > 0)
     {
         console.log(results);
-        console.log("x value of nose"+ results[0].pose.nose.x);
-        console.log("y value of nose"+ results[0].pose.nose.y);
+        console.log("x value of left_ear"+ results[0].pose.leftEar.x);
+        console.log("y value of left_ear"+ results[0].pose.leftEar.y);
+        left_earx = results[0].pose.leftEar.x - 260;
+        left_eary = results[0].pose.leftEar.y - 280;
+
+        console.log("x value of right_ear"+ results[0].pose.rightEar.x);
+        console.log("y value of right_ear"+ results[0].pose.rightEar.y);
+        right_earx = results[0].pose.rightEar.x - 170;
+        right_eary = results[0].pose.rightEar.y - 275;
+
+        console.log("x value of leftEye"+ results[0].pose.leftEye.x);
+        console.log("y value of leftEye"+ results[0].pose.leftEye.y);
+        glasses_x = results[0].pose.leftEye.x - 280;
+        glasses_y = results[0].pose.leftEye.y - 135;
     }
 }
+
